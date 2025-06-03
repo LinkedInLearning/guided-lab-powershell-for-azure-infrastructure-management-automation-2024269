d4a1895dd4b7b7043285823d10d5c14c01006c2e
jpomfret <jpomfret7@gmail.com>
2025-06-03 09:55:40 +0000

    install pester

---
+++ code/04_01/demo.ps1
@@ -0,0 +1,8 @@
+# View currently versions available
+Get-Module Pester -ListAvailable
+
+# Find latest module on PowerShell Gallery
+Find-PsResource -Name Pester
+
+# Install latest version
+Install-PSResource -Name Pester
