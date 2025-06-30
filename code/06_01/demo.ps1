87508d1a8922e5381366b93f84563014211ab404
jpomfret <jpomfret7@gmail.com>
2025-06-30 16:25:01 +0000

    also get the tag data

--- code/06_01/demo.ps1
+++ code/06_01/demo.ps1
@@ -1,4 +1,4 @@
-Set-Location .\code\06-01
+Set-Location .\code\06_01
 
 # Add a helper function to convert column letters to numbers
 # Convert column letter to number
@@ -31,3 +31,24 @@ $importParams = @{
     StartColumn    = $column
 }
 $data = Import-Excel @importParams
+
+# Also find the address of the Info table - this will become our tags
+$e = Open-ExcelPackage -Path .\ProjectTemplate.xlsx
+$address = $e.Workbook.Worksheets['ProjectTemplate'].Tables['Info'].Address
+$startCell = $address.Address.split(':')[0]
+$endCell = $address.Address.split(':')[1]
+$startColumn = ConvertTo-ExcelColumnNumber -columnLetter ([regex]::Match($startCell, "[A-Za-z]+").Value)
+$startRow = [regex]::Match($startCell, "\d+").Value
+$endColumn = ConvertTo-ExcelColumnNumber -columnLetter ([regex]::Match($endcell, "[A-Za-z]+").Value)
+$endRow = [regex]::Match($endcell, "\d+").Value
+
+# Import the spreadsheet
+$importParams = @{
+    Path           = '.\ProjectTemplate.xlsx'
+    WorksheetName  = 'ProjectTemplate'
+    StartRow       = $startRow
+    EndRow         = $endRow
+    StartColumn    = $startColumn
+    EndColumn      = $endColumn
+}
+$tags = Import-Excel @importParams
