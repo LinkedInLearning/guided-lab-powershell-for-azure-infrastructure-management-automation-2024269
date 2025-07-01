d069c5c2ffaee55a268227c940f6d60e3c3defa8
jpomfret <jpomfret7@gmail.com>
2025-07-01 13:50:55 +0000

    check for tags

--- code/06_03/tests/generic.tests.ps1
+++ code/06_03/tests/generic.tests.ps1
@@ -1,17 +1,29 @@
 param (    
     [Parameter(Mandatory)]
-    [string]$rgName
-)
+    [string]$rgName,
+    
+    [Parameter(Mandatory)]
+    [Hashtable]$tag
+) 
 Describe "Azure Resources in resource group $rgName have tags" {
-    BeforeAll {
-        $res = Get-AzResource -ResourceGroupName $rgName
-        $res | ogv
-        $requiredTags = @('environment','author','costs') 
-    }        
-    Context '<_.Name> should have tags'  -Foreach $res {   
-        It "resource should have <_> tag" -Foreach  @('environment','author','costs')  {
-            $_.Tags.Keys | Should -Contain $_
+    $res = Get-AzResource -ResourceGroupName $rgName | Select-Object Name, ResourceId, Tags
+    Context '<_.Name> should have tags' -Foreach $res { 
+        BeforeAll {
+            $resource = $_
+        }
+
+        It "<_.Name> should have tags" {
+            $resource.Tags | Should -Not -BeNullOrEmpty
+        }
+        
+        It "resource should have <tagName> tag" -Foreach @(
+            @{tagName = 'environment'},
+            @{tagName = 'author'},
+            @{tagName = 'costs'}
+        )  {
+        #-Skip:($null -eq $resource.Tags)
+            # We already checked if Tags exists in a separate test, so we can safely access Keys here
+            $resource.Tags.Keys | Should -Contain $tagName
         }
     }
-
 }
